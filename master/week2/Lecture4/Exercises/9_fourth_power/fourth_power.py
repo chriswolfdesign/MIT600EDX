b975e39f9cb1c1a82792f203380f870d6d499676
Chris Wolf <chriswolf@Chriss-MacBook-Pro.local>
2017-07-23 15:10:48 -0400

    "Completed (success): Lecture 4 Exercise 9"

---
+++ master/week2/Lecture4/Exercises/9_fourth_power/fourth_power.py
@@ -0,0 +1,30 @@
+"""
+Used for running, do not include when submitting to grader.
+"""
+
+def square(x):
+    """
+    x: int or float
+    return: x^2
+    """
+    return x * x
+"""
+End Runner Code
+"""
+
+def fourthPower(x):
+    """
+    Input:
+    x: int or float.
+    Returns: x ^ 4
+    """
+    return square(x) * square(x)
+
+"""
+Used for running, do not include when submitting to the grader.
+"""
+x = 2.0
+print(fourthPower(x))
+"""
+End Runner Code
+"""
